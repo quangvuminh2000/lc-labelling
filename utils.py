da639733c325959b7a10d18dcf344124b2939ef9
VU MINH QUANG <quangvm9@Henry-Quangs-MacBook-Pro.local>
2023-11-03 09:21:30 +0700

    Add missing utils functions

--- utils.py
+++ utils.py
@@ -5,6 +5,10 @@ def save_data_gcs(data_path: str, to_path: str, conn: FilesConnection):
     conn.fs.put_file(data_path, to_path)
 
 
+def get_data_gcs(data_path: str, to_path: str, conn: FilesConnection):
+    conn.fs.get_file(data_path, to_path)
+
+
 def load_data_gcs(data_path: str, conn: FilesConnection):
     df = conn.read(data_path, input_format="csv", ttl=60)
 
