eb4afac673c2549185f07e38dddd32f40aa78887
VU MINH QUANG <quangvm9@Henry-Quangs-MacBook-Pro.local>
2023-11-03 14:35:14 +0700

    Fix layout header login and main page

--- main_page.py
+++ main_page.py
@@ -10,12 +10,16 @@ from auth import authentication_component
 from label_page import labelling_component
 
 if __name__ == "__main__":
+    labelling_component()
+
+    authentication_component()
+
     st.markdown(
         """
         <style>
             .block-container {
-                padding-top: 0rem;
-                padding-bottom: 0rem;
+                padding-top: 3rem;
+                padding-bottom: 1rem;
                 padding-left: 1rem;
                 padding-right: 1rem;
                 # margin-top: -2rem;
@@ -24,7 +28,3 @@ if __name__ == "__main__":
         """,
         unsafe_allow_html=True,
     )
-
-    authentication_component()
-
-    labelling_component()
